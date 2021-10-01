a99900538ea19f4982cf5750dbeff693a4026748
khanhnguyenvinh2002 <giakhanhvy@gmail.com>
2021-09-30 20:39:38 -0400

    update

---
+++ assets/BrandeisMajor.js
@@ -0,0 +1,21 @@
+export default function BrandeisMajor(){
+  return (
+[
+{
+"title": "Computer Science",
+"embeddedMajor":"Computer Science"
+},
+{
+"title": "Business",
+"embeddedMajor":"Business"
+},
+{
+"title": "Economics",
+"embeddedMajor":"Economics"
+},
+{
+"title": "Mathematics",
+"embeddedMajor":"Mathematics"
+},
+]
+)}
